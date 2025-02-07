c5d9e15c0e7aa0141d8b53a91d7869b992c27d28
Lê Khải Hoàn <129830151+yuu-erp@users.noreply.github.com>
2025-02-07 23:24:15 +0700

    update

---
+++ src/core/application/mappers/mapper.base.ts
@@ -0,0 +1,11 @@
+import { Entity } from "@core/domain/entities";
+
+export interface Mapper<
+  DomainEntity extends Entity<unknown>,
+  DbRecord,
+  Response = unknown
+> {
+  toPersistence(entity: DomainEntity): DbRecord;
+  toDomain(record: DbRecord): DomainEntity;
+  toResponse(entity: DomainEntity): Response;
+}
